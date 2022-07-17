4c4cdb8f7b4eaea8a2b37ac2198f332374429ce3
Edward Rees <erees.hk@gmail.com>
2022-07-16 21:17:25 -0700

    port?

--- pm2.config.js
+++ pm2.config.js
@@ -1,33 +1,34 @@
-const name="todolist.edwardrees.info";
-const user="erees";
-const host="todolist.edwardrees.info";
-const port=5750;
-const build="yarn install; yarn build";
+const name = "todolist.edwardrees.info";
+const user = "erees";
+const host = "todolist.edwardrees.info";
+const port = 5750;
+const build = "yarn install; yarn build";
 
 module.exports = {
   apps: [
     {
       name,
-      script: 'yarn server',
+      script: "yarn server",
       args: `--port ${port}`,
       env: {
-        NODE_ENV: 'development'
+        PORT: 5500,
+        NODE_ENV: "development",
       },
       env_production: {
-        NODE_ENV: 'production',
-        PORT: port
-      }
-    }
+        PORT: 5750,
+        NODE_ENV: "production",
+      },
+    },
   ],
   deploy: {
     production: {
-      user: 'root',
-      host: '74.207.247.118',
-      ref: 'origin/master',
-      repo: 'git@github.com:EdwardRees/Todo-Project.git',
-      path: '/var/www/html/todolist.edwardrees.info',
-      ssh_options: 'ForwardAgent=yes',
-      'post-deploy': `${build} && pm2 reload ecosystem.config.js --env production && pm2 save`
-    }
-  }
-}
+      user: "root",
+      host: "74.207.247.118",
+      ref: "origin/master",
+      repo: "git@github.com:EdwardRees/Todo-Project.git",
+      path: "/var/www/html/todolist.edwardrees.info",
+      ssh_options: "ForwardAgent=yes",
+      "post-deploy": `${build} && pm2 reload ecosystem.config.js --env production && pm2 save`,
+    },
+  },
+};
